e751fc4b13f7262725cfaff1a5d7e4fb7be5793f
Ross Weinstein <ross.a.weinstein@gmail.com>
2017-07-05 11:42:57 -0500

    test passed

--- src/model/patron/patronInfo/ContactInfo.java
+++ src/model/patron/patronInfo/ContactInfo.java
@@ -1,5 +1,19 @@
 package model.patron.patronInfo;
 
 public class ContactInfo {
+	
+	private String firstName;
+	
+	public ContactInfo() {
+		this.firstName = "";
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
 }
