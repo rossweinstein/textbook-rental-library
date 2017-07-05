8d7933d2caefc0409eda662f81a2322d13bc5cd5
Ross Weinstein <ross.a.weinstein@gmail.com>
2017-07-05 10:12:28 -0500

    test passed

--- src/model/patron/patronInfo/Address.java
+++ src/model/patron/patronInfo/Address.java
@@ -3,19 +3,19 @@ package model.patron.patronInfo;
 import java.util.Arrays;
 
 public class Address {
-	
+
 	private String addressLineOne;
 	private String addressLineTwo;
 	private String city;
 	private String state;
-	
+
 	public Address() {
 		this.addressLineOne = "";
 		this.addressLineTwo = "";
 		this.city = "";
 		this.state = "";
 	}
-	
+
 	public String getAddressLineOne() {
 		return this.addressLineOne;
 	}
@@ -23,37 +23,45 @@ public class Address {
 	public void setAddressLineOne(String lineOne) {
 		this.addressLineOne = lineOne;
 	}
-	
+
 	public String getAddressLineTwo() {
 		return this.addressLineTwo;
 	}
-	
+
 	public void setAddressLineTwo(String lineTwo) {
 		this.addressLineOne = lineTwo;
 	}
-	
+
 	public String getState() {
 		return this.state;
 	}
-	
+
 	public void setState(String state) {
-		
+
 		if (this.validState(state)) {
 			this.state = state;
 		} else {
 			this.state = "";
 		}
 	}
-	
+
 	public boolean validState(String state) {
 		return Arrays.stream(USStates.values()).anyMatch(usStates -> usStates.equals(state));
 	}
-	
+
 	public String getCity() {
 		return this.city;
 	}
-	
+
 	public void setCity(String city) {
-		this.city = city;
+		if (this.validCity(city)) {
+			this.city = city;
+		} else {
+			this.city = "";
+		}
+	}
+
+	public boolean validCity(String city) {
+		return city.chars().allMatch(Character::isLetter);
 	}
 }
