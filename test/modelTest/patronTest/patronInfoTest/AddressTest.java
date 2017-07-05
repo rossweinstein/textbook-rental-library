476473676901cf12422a229302ecbf7343caf18a
Ross Weinstein <ross.a.weinstein@gmail.com>
2017-07-05 09:40:34 -0500

    test passes - created Address class

---
+++ test/modelTest/patronTest/patronInfoTest/AddressTest.java
@@ -0,0 +1,19 @@
+package modelTest.patronTest.patronInfoTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import model.patron.patronInfo.Address;
+
+public class AddressTest {
+
+	
+	private Address address;
+	
+	@Test
+	public void createNewAddressClass() {
+		this.address = new Address();
+	}
+
+}
