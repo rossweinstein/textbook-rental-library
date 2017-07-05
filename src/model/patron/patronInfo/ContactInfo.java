30bdffd6518a1b38b890c2d304777746a415ac9c
Ross Weinstein <ross.a.weinstein@gmail.com>
2017-07-05 11:46:45 -0500

    test passed

--- src/model/patron/patronInfo/ContactInfo.java
+++ src/model/patron/patronInfo/ContactInfo.java
@@ -4,10 +4,12 @@ public class ContactInfo {
 	
 	private String firstName;
 	private String lastName;
+	private String phoneNumber;
 	
 	public ContactInfo() {
 		this.firstName = "";
 		this.lastName = "";
+		this.phoneNumber = "";
 	}
 
 	public String getFirstName() {
@@ -24,6 +26,14 @@ public class ContactInfo {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
 	}
 
 	
