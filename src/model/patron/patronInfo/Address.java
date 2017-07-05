446293c0f69b46ffd1e42cfdcb4569038c2eba5a
Ross Weinstein <ross.a.weinstein@gmail.com>
2017-07-05 09:44:29 -0500

    test passed - added setter to Address class

--- src/model/patron/patronInfo/Address.java
+++ src/model/patron/patronInfo/Address.java
@@ -2,5 +2,11 @@ package model.patron.patronInfo;
 
 public class Address {
 	
+	private String addressLineOne;
+	
 	public Address() {}
+
+	public void setAddressLineOne(String lineOne) {
+		this.addressLineOne = lineOne;
+	}
 }
