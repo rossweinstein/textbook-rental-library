526579a635a29dbd525a9e60e3e8f0adc028c22e
Ross Weinstein <ross.a.weinstein@gmail.com>
2017-07-05 09:46:49 -0500

    refactored

--- src/model/patron/patronInfo/Address.java
+++ src/model/patron/patronInfo/Address.java
@@ -5,7 +5,11 @@ public class Address {
 	private String addressLineOne;
 	private String addressLineTwo;
 	
-	public Address() {}
+	public Address() {
+		this.addressLineOne = "";
+		this.addressLineTwo = "";
+		
+	}
 
 	public void setAddressLineOne(String lineOne) {
 		this.addressLineOne = lineOne;
