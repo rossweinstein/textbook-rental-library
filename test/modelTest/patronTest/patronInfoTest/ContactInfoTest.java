df5bb934fe7b8be85f98735a8bed730db4b91a5a
Ross Weinstein <ross.a.weinstein@gmail.com>
2017-07-05 11:39:28 -0500

    test failed - contactInfo

---
+++ test/modelTest/patronTest/patronInfoTest/ContactInfoTest.java
@@ -0,0 +1,14 @@
+package modelTest.patronTest.patronInfoTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContactInfoTest {
+
+	@Test
+	public void createNewContactInfo() {
+		ContactInfo contact = new ContactInfo();
+	}
+
+}
