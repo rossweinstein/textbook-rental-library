ae97dbbaacf81d76bddd6b7df20fc12e43dd7147
Ross Weinstein <ross.a.weinstein@gmail.com>
2017-07-05 11:41:56 -0500

    refactored, failed getFirstName test

--- test/modelTest/patronTest/patronInfoTest/ContactInfoTest.java
+++ test/modelTest/patronTest/patronInfoTest/ContactInfoTest.java
@@ -2,15 +2,23 @@ package modelTest.patronTest.patronInfoTest;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import model.patron.patronInfo.ContactInfo;
 
 public class ContactInfoTest {
 
-	@Test
+	ContactInfo contact;
+	
+	@Before
 	public void createNewContactInfo() {
-		ContactInfo contact = new ContactInfo();
+		this.contact = new ContactInfo();
+	}
+	
+	@Test
+	public void getFirstName() {
+		this.contact.getFirstName();
 	}
 
 }
